7d86e857708e62182e287c05afbf06e5d59fab35
Rajiv Sharma <sharma9769387961@gmail.com>
2025-03-28 17:14:22 +0530

    All Setup Config

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import { app } from "./app.js";
 
 connectDB()
   .then(()=>{
-    app.on("errror", (error) => {
-      console.log("ERRR: ", error);
+    app.on("error", (error) => {
+      console.log("Error: ", error);
       throw error
     });
     app.listen(process.env.PORT || 8000,()=>{
-      console.log("Server is running on port 8080");
+      console.log("Server is running on port ",process.env.PORT);
     })
   })
   .catch(error =>{
---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,33 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs';
+import upload from "../middlewares/multer.middleware.js";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (filePath) => {
+    try{
+        if (!fs.existsSync(filePath)) return 'File not found';
+        const uploadResult = await cloudinary.uploader
+            .upload(
+                filePath,
+                {
+                    resource_type: 'auto',
+                }
+            )
+        // console.log('File successfully uploaded on URL:',uploadResult.url);
+        fs.unlinkSync(filePath);
+        return uploadResult;
+    }
+    catch (error) {
+        fs.unlinkSync(filePath);
+        console.log(error);
+        return null;
+    }
+}
+
+export default uploadOnCloudinary;
+
