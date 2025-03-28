1935ace285cbef3a9db1dc4573b2eeca81f10e08
Rajiv Sharma <sharma9769387961@gmail.com>
2025-03-28 11:52:10 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import connectDB from './db/index.js';
 import 'dotenv/config';
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+  .then(()=>{
+    app.on("errror", (error) => {
+      console.log("ERRR: ", error);
+      throw error
+    });
+    app.listen(process.env.PORT || 8000,()=>{
+      console.log("Server is running on port 8080");
+    })
+  })
+  .catch(error =>{
+    console.log(error);
+  });
